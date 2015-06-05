955998d9e3e41c142459d3acb4068e8aeb84c0de
David Goyard <david.goyard@cea.fr>
2015-06-05 14:36:03 +0200

    synchronisation of files

---
+++ piws/scripts/tools.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+##########################################################################
+# NSAp - Copyright (C) CEA, 2013
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
+
+
+# Tools for the CubicWeb instance
+
+
+
+#/neurospin/eu-aims/PROCESS/MRI/LEAP_V01/112250651739/anatomy/Adni_sag.nii.gz
+#ajax?fname=get_slices_from_nifti&arg=titi&arg=100
